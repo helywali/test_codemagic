56502764f2e082e4b75d0fbcc3a13a0da2a73122
sohila wali <93332399+sohila171@users.noreply.github.com>
2023-10-24 13:22:48 +0300

    .

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,8 +1,8 @@
 import UIKit
 import Firebase
 import Flutter
-import GoogleMaps
 
+//import os.log
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -11,11 +11,6 @@ import GoogleMaps
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     FirebaseApp.configure()
-    RemoteConfig.remoteConfig().fetchAndActivate() { status, error in
-      let apiKey : String = RemoteConfig.remoteConfig()["Google_Maps_SDK_for_iOS_API_KEY"].stringValue ?? "MISSING";
-      // os_log("Google_Maps_SDK_for_iOS_API_KEY = '%@'", apiKey)
-      GMSServices.provideAPIKey(apiKey)
-    }
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
